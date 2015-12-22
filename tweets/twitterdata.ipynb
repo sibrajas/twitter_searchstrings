79e6b60699ff35dbe75ba14b090b0a3e93430203
Sibi Rajasekaran <log2sibi@gmail.com>
2015-12-22 15:51:22 +0000

    Twitter stream search for a string

---
+++ tweets/twitterdata.ipynb
@@ -0,0 +1,5 @@
+import urllib
+import json
+response=urllib.urlopen("http://www.search.twitter.com/search.json?q=microsoft")
+#pyresponse=json.dumps(response)
+print response
---
+++ twitter-python/auth.py
@@ -0,0 +1,11 @@
+
+class TwitterAuth:
+	# Go to http://dev.twitter.com and create an app. 
+	# The consumer key and secret will be generated for you after
+	consumer_key="tGqU0Vxhu7BKJGBkZCmgcLvFw"
+	consumer_secret="laDFi4Bc9nKKoSqUUSxk5nqhUUg7IFzFiZuW2AxiUgn0I13fBZ"
+
+	# After the step above, you will be redirected to your app's page.
+	# Create an access token under the the "Your access token" section
+	access_token="116246545-ambic09VtySKF1Gj4sEfvlEvShRuWpMzKhuXEpX0"
+	access_token_secret="mUQDz7yJoIMVKFygbczwkUr3dYHM3K1772Y4Kt3f2Hp03"
